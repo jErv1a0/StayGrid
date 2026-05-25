ba3bec5388d287bdc7f179eafe09b42485798d21
Jervine Alvarico <jervinealvarico@gmail.com>
2026-05-25 14:57:35 +0800

    Redirect logged-in users away from public home

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -11,6 +11,18 @@ class LandingController extends AbstractController
     #[Route('/landing', name: 'app_landing')]
     public function index(): Response
     {
+        if ($this->getUser()) {
+            if ($this->isGranted('ROLE_ADMIN')) {
+                return $this->redirectToRoute('app_admin_home');
+            }
+
+            if ($this->isGranted('ROLE_STAFF')) {
+                return $this->redirectToRoute('app_staff_dashboard');
+            }
+
+            return $this->redirectToRoute('app_user_dashboard');
+        }
+
         return $this->render('landingpage/landing.html.twig');
     }
 }
