0c83fc0fe4ce0bc8065ab421e03abd3c3acaaa08
Jervine Alvarico <jervinealvarico@gmail.com>
2026-05-25 16:14:09 +0800

    Allow user feedback page to bypass generic user access control

--- src/Controller/User/UserFeedbackController.php
+++ src/Controller/User/UserFeedbackController.php
@@ -9,10 +9,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/user/feedback')]
-#[IsGranted('ROLE_CLIENT')]
 class UserFeedbackController extends AbstractController
 {
     #[Route('', name: 'app_user_feedback', methods: ['GET', 'POST'])]
