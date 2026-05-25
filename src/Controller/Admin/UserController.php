f49c7c5eec31962924c836e910d7b32d2b7a4c56
Jervine Alvarico <jervinealvarico@gmail.com>
2026-05-25 12:02:47 +0800

    Add admin user edit and delete actions

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -4,8 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\LogInUsers;
 use App\Repository\LogInUsersRepository;
+use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,14 +18,62 @@ class UserController extends AbstractController
     #[Route('/', name: 'app_admin_users_index')]
     public function index(LogInUsersRepository $userRepository): Response
     {
-        // Option A (simple): show all users
         $users = $userRepository->findAll();
-
-        // Option B (if you have isVerified field)
-        // $users = $userRepository->findBy(['isVerified' => true]);
 
         return $this->render('admin/users/index.html.twig', [
             'users' => $users,
+        ]);
+    }
+
+    #[Route('/new', name: 'app_admin_users_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new LogInUsers();
+        $form = $this->createForm(UserType::class, $user, ['is_edit' => false]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('password')->getData();
+            if (is_string($plainPassword) && $plainPassword !== '') {
+                $user->setPassword($passwordHasher->hashPassword($user, $plainPassword));
+            }
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', sprintf('User %s has been created.', $user->getEmail()));
+
+            return $this->redirectToRoute('app_admin_users_index');
+        }
+
+        return $this->render('admin/users/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_admin_users_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, LogInUsers $user, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $form = $this->createForm(UserType::class, $user, ['is_edit' => true]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('password')->getData();
+            if (is_string($plainPassword) && $plainPassword !== '') {
+                $user->setPassword($passwordHasher->hashPassword($user, $plainPassword));
+            }
+
+            $entityManager->flush();
+
+            $this->addFlash('success', sprintf('User %s has been updated.', $user->getEmail()));
+
+            return $this->redirectToRoute('app_admin_users_index');
+        }
+
+        return $this->render('admin/users/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
         ]);
     }
 
@@ -59,6 +109,24 @@ class UserController extends AbstractController
         $entityManager->flush();
 
         $this->addFlash('success', sprintf('User %s has been marked unverified.', $user->getEmail()));
+
+        return $this->redirectToRoute('app_admin_users_index');
+    }
+
+    #[Route('/{id}/delete', name: 'app_admin_users_delete', methods: ['POST'])]
+    public function delete(
+        LogInUsers $user,
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response {
+        if (!$this->isCsrfTokenValid('delete_user_' . $user->getId(), (string) $request->request->get('_token'))) {
+            throw $this->createAccessDeniedException('Invalid CSRF token.');
+        }
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        $this->addFlash('success', sprintf('User %s has been deleted.', $user->getEmail()));
 
         return $this->redirectToRoute('app_admin_users_index');
     }
